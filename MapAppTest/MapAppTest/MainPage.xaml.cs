5b6ea845e0d3d64f995a36c4ff5fdc472b2793bb
Jeremy Prater <Jeremy Prater>
2017-05-27 12:39:11 -0700

    Add project files.

---
+++ MapAppTest/MapAppTest/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace MapAppTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.MyMap.MoveToRegion(
+                MapSpan.FromCenterAndRadius(
+                    new Position(37, -122), Distance.FromMiles(1)));
+
+        }
+    }
+}
