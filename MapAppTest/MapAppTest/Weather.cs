6314269fb71293c927182c8a2a3af931df71083e
Ben Walton <waltonb@oregonstate.edu>
2017-06-02 14:13:09 -0500

    Add Weather.cs to project. Got lot's of errors about a values.xml file. I didn't make any changes to any xml files.

---
+++ MapAppTest/MapAppTest/Weather.cs
@@ -0,0 +1,194 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapAppTest
+{
+    public class Weather
+    {
+            public string Title { get; set; }
+            public string Temperature { get; set; }
+            public string Wind { get; set; }
+            public string Humidity { get; set; }
+            public string Visibility { get; set; }
+            public string Sunrise { get; set; }
+            public string Sunset { get; set; }
+
+            public Weather()
+            {
+                this.Title = " ";
+                this.Temperature = " ";
+                this.Wind = " ";
+                this.Humidity = " ";
+                this.Visibility = " ";
+                this.Sunrise = " ";
+                this.Sunset = " ";
+            }
+    }
+
+    public class Features
+    {
+        public int conditions { get; set; }
+    }
+
+    public class Response
+    {
+        public string version { get; set; }
+        public string termsofService { get; set; }
+        public Features features { get; set; }
+    }
+
+    public class Image
+    {
+        public string url { get; set; }
+        public string title { get; set; }
+        public string link { get; set; }
+    }
+
+    public class DisplayLocation
+    {
+        public string full { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string state_name { get; set; }
+        public string country { get; set; }
+        public string country_iso3166 { get; set; }
+        public string zip { get; set; }
+        public string magic { get; set; }
+        public string wmo { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string elevation { get; set; }
+    }
+
+    public class ObservationLocation
+    {
+        public string full { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+        public string country_iso3166 { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string elevation { get; set; }
+    }
+
+    public class Estimated
+    {
+    }
+
+    public class CurrentObservation
+    {
+        public Image image { get; set; }
+        public DisplayLocation display_location { get; set; }
+        public ObservationLocation observation_location { get; set; }
+        public Estimated estimated { get; set; }
+        public string station_id { get; set; }
+        public string observation_time { get; set; }
+        public string observation_time_rfc822 { get; set; }
+        public string observation_epoch { get; set; }
+        public string local_time_rfc822 { get; set; }
+        public string local_epoch { get; set; }
+        public string local_tz_short { get; set; }
+        public string local_tz_long { get; set; }
+        public string local_tz_offset { get; set; }
+        public string weather { get; set; }
+        public string temperature_string { get; set; }
+        public double temp_f { get; set; }
+        public double temp_c { get; set; }
+        public string relative_humidity { get; set; }
+        public string wind_string { get; set; }
+        public string wind_dir { get; set; }
+        public int wind_degrees { get; set; }
+        public double wind_mph { get; set; }
+        public string wind_gust_mph { get; set; }
+        public double wind_kph { get; set; }
+        public string wind_gust_kph { get; set; }
+        public string pressure_mb { get; set; }
+        public string pressure_in { get; set; }
+        public string pressure_trend { get; set; }
+        public string dewpoint_string { get; set; }
+        public int dewpoint_f { get; set; }
+        public int dewpoint_c { get; set; }
+        public string heat_index_string { get; set; }
+        public string heat_index_f { get; set; }
+        public string heat_index_c { get; set; }
+        public string windchill_string { get; set; }
+        public string windchill_f { get; set; }
+        public string windchill_c { get; set; }
+        public string feelslike_string { get; set; }
+        public string feelslike_f { get; set; }
+        public string feelslike_c { get; set; }
+        public string visibility_mi { get; set; }
+        public string visibility_km { get; set; }
+        public string solarradiation { get; set; }
+        public string UV { get; set; }
+        public string precip_1hr_string { get; set; }
+        public string precip_1hr_in { get; set; }
+        public string precip_1hr_metric { get; set; }
+        public string precip_today_string { get; set; }
+        public string precip_today_in { get; set; }
+        public string precip_today_metric { get; set; }
+        public string icon { get; set; }
+        public string icon_url { get; set; }
+        public string forecast_url { get; set; }
+        public string history_url { get; set; }
+        public string ob_url { get; set; }
+        public string nowcast { get; set; }
+    }
+
+    public class RootObject
+    {
+        public Response response { get; set; }
+        public CurrentObservation current_observation { get; set; }
+    }
+
+    public class Core
+    {
+        public static async Task<Weather> GetWeather(double lat, double lon)
+        {
+            string key = "30ae4f88a9679843";
+            string queryString = $"http://api.wunderground.com/api/{key}/conditions/q/{lat},{lon}.json";
+
+            RootObject results = await DataServices.getDataFromServices(queryString).ConfigureAwait(false);
+
+            if (results != null)
+            {
+                Weather weather = new Weather();
+                weather.Title = results.current_observation.display_location.full;
+                weather.Temperature = results.current_observation.temperature_string;
+                weather.Wind = results.current_observation.wind_string;
+                weather.Humidity = results.current_observation.relative_humidity;
+                weather.Visibility = results.current_observation.visibility_mi + " miles";
+
+                return weather;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+
+    public class DataServices
+    {
+        public static async Task<RootObject> getDataFromServices(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            RootObject data = null;
+            if (response != null)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject<RootObject>(json);
+            }
+
+            return data;
+        }
+    }
+}
