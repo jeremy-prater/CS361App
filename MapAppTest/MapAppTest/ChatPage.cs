fc5e95b35c4068b78d5c7498a404765cc3fb601e
Jonathan Horton <hortonjo@oregonstate.edu>
2017-06-07 17:54:14 -0400

    Added a few buttons to the Chat tab to show off how the Xamarin messaging system works -- still no way to communicate with others.

---
+++ MapAppTest/MapAppTest/ChatPage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MapAppTest
+{
+	public class ChatPage : ContentPage
+	{
+		public ChatPage ()
+		{
+            // A lot of this content comes from the provided Xamarin messaging example
+
+            BindingContext = new ChatPageViewModel();
+
+            Label header = new Label
+            {
+                Text = "Inter-Program Testing",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(EntryCell)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            // Need to define our special text box before we render it in a table
+            var textBox = new EntryCell
+            {
+                Label = "Name:",
+                Placeholder = "John Doe",
+                Text = "Instructor Rooker",
+                Keyboard = Keyboard.Default
+            };
+
+            TableView tableView = new TableView
+            {
+                Intent = TableIntent.Form,
+                Root = new TableRoot
+                {
+                    new TableSection
+                    {
+                        textBox
+                    }
+                }
+            };
+
+            // Send messages when buttons are pressed
+            var button1 = new Button { Text = "Say Hi" };
+            button1.Clicked += (sender, e) => {
+                MessagingCenter.Send<ChatPage>(this, "Hi");
+            };
+            var button2 = new Button { Text = "Say Hi to me!"};
+            button2.Clicked += (sender, e) => {
+                MessagingCenter.Send<ChatPage, string>(this, "Hi", textBox.Text);
+            };
+
+            var button3 = new Button { Text = "Unsubscribe from alert" };
+            button3.Clicked += (sender, e) => {
+                MessagingCenter.Unsubscribe<ChatPage, string>(this, "Hi");
+                DisplayAlert("Unsubscribed",
+                    "This page has stopped listening, so no more alerts; however the ViewModel is still receiving messages.",
+                    "OK");
+            };
+
+            // Subscribe to a message (which the ViewModel has also subscribed to) to pop up an Alert
+            MessagingCenter.Subscribe<ChatPage, string>(this, "Hi", (sender, arg) => {
+                DisplayAlert("Message Received", "arg=" + arg, "OK");
+            });
+
+            var listView = new ListView();
+            listView.SetBinding(ListView.ItemsSourceProperty, "Greetings");
+
+            Content = new StackLayout
+            {
+                //Padding = new Thickness(0, 20, 0, 0),
+                Children = { header, tableView, button1, button2, button3, listView }
+            };
+        }
+	}
+}
---
+++ MapAppTest/MapAppTest/ChatPageViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MapAppTest
+{
+	public class ChatPageViewModel : ContentView
+	{
+        public ObservableCollection<string> Greetings { get; set; }
+
+        public ChatPageViewModel()
+        {
+            Greetings = new ObservableCollection<string>();
+
+            MessagingCenter.Subscribe<ChatPage>(this, "Hi", (sender) =>
+            {
+                Greetings.Add("Hi");
+            });
+
+            MessagingCenter.Subscribe<ChatPage, string>(this, "Hi", (sender, arg) =>
+            {
+                Greetings.Add("Hi " + arg);
+            });
+        }
+    }
+}
