1fee438fef68167825588cfdf1d90ee39ce91faf
Jeff <jeffreyvluong@gmail.com>
2017-06-08 23:14:39 -0700

    Added Unit Tests for Weather App. Will add the rest tomorrow morning

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MapAppTest.Droid;
 using MapAppTest;
 using Xamarin.Forms.Maps;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace UnitTestProject1
 {
@@ -25,5 +27,31 @@ namespace UnitTestProject1
             Assert.AreEqual(newEngine.GetLongitudeDegrees(), testLong);
             //Assert.IsTrue(false); // Test assert
         }
-    }
-}
+
+        [TestMethod]
+        public async Task TestWeatherRetrieval()
+        {
+            // Grab weather for Hillcrest, San Diego :)
+            Weather currentWeather = await Core.GetCurrentWeather(32.7497888, -117.1676501);
+            Assert.IsNotNull(currentWeather);
+
+            // Debug to verify we get back data
+            //Console.WriteLine(currentWeather.Title);
+            //Console.WriteLine(currentWeather.Temperature);
+            //Console.WriteLine(currentWeather.Wind);
+            //Console.WriteLine(currentWeather.Humidity);
+            //Console.WriteLine(currentWeather.Visibility);
+        } // TestWeatherRetrieval()
+
+        [TestMethod]
+        public async Task TestWeatherReturnTitle()
+        {
+            // Testing to make sure we are getting data for San Diego, CA
+            Weather currentWeather = await Core.GetCurrentWeather(32.7497888, -117.1676501);
+            // Testing to make sure we are getting data for San Diego, CA
+            Assert.AreEqual(currentWeather.Title, "San Diego, CA");
+        }
+
+        //TODO: Add unit tests for the rest of the data points JL 6/8/2017
+    } // UnitTest1
+} // UnitTestProject1
