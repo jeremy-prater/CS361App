b9d56cbeef7c89b69fed3a667ab1b2489401d01b
Jeremy Prater <shady8860@hotmail.com>
2017-06-09 23:25:41 -0700

    - Attempt to add weather to trail data, but the async results hang the app

--- MapAppTest/MapAppTest/TrailViewer.xaml.cs
+++ MapAppTest/MapAppTest/TrailViewer.xaml.cs
@@ -25,6 +25,20 @@ namespace MapAppTest
             location.Text = matchedTrail.city + " ," + matchedTrail.state;
             description.Text = matchedTrail.description;
 
+
+            Task<Weather> currentWeatherTask = WeatherCore.GetCurrentWeather(matchedTrail.lat, matchedTrail.lon);
+            currentWeatherTask.Wait();
+            Weather currentWeather = currentWeatherTask.Result;
+
+            Task<ForecastWeather> currentForecastTask = WeatherCore.GetCurrentForecast(matchedTrail.lat, matchedTrail.lon);
+            currentForecastTask.Wait();
+            ForecastWeather currentForecast = currentForecastTask.Result;
+
+            currentTemp.Text = "Current Temp : " + currentWeather.Temperature;
+            currentRain.Text = "Current Rainfall : " + currentForecast.ForecastDay;
+
+
+
             // Implement which activities are available at current trail
             //
             // foreach (TrailActivity activity in matchedTrail.activities)
